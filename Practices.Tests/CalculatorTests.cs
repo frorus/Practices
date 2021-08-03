d1b9c0cf088183ee2c4cfc1705888244de8476e3
Ilya Silkin <zerolar@gmail.com>
2021-08-03 22:43:20 +0300

    Добавьте файлы проекта.

---
+++ Practices.Tests/CalculatorTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Practices.Tests
+{
+    [TestFixture]
+    internal class CalculatorTests
+    {
+        [Test]
+        public void Additional_MustReturnCorrectValue()
+        {
+            Calculator calculator = new();
+            Assert.That(calculator.Additional(300, 10) == 310);
+        }
+
+        [Test]
+        public void Subtraction_MustReturnCorrectValue()
+        {
+            Calculator calculator = new();
+            Assert.That(calculator.Subtraction(300, 10) == 290);
+        }
+
+        [Test]
+        public void Multiplication_MustReturnCorrectValue()
+        {
+            Calculator calculator = new();
+            Assert.That(calculator.Multiplication(300, 10) == 3000);
+        }
+
+        [Test]
+        public void Division_MustReturnCorrectValue()
+        {
+            Calculator calculator = new();
+            Assert.That(calculator.Division(300, 10) == 30);
+        }
+
+        [Test]
+        public void Division_MustThrowException()
+        {
+            Calculator calculator = new();
+            Assert.Throws<DivideByZeroException>(() => calculator.Division(30, 0));
+        }
+    }
+}
---
+++ Practices/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Practices
+{
+    public class Calculator
+    {
+        public int Additional(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
